b738d1ceb11b5e640793b57700b761db952ae308
si-mv <simon.haines@multiverse.io>
2022-10-23 22:01:59 +0100

    handles error

--- server/controllers/request.route.js
+++ server/controllers/request.route.js
@@ -3,11 +3,13 @@ const router = express.Router()
 const { Request } = require('../models')
 
 router.get('/:id', async (req, res) => {
-  console.log('Got a danged request!')
-  const request = await Request.findByPk(req.params.id)
-  console.log(request)
-  if (!request) return res.sendStatus(404)
-  return res.status(200).send(request)
+  try {
+    const request = await Request.findByPk(req.params.id)
+    if (!request) return res.sendStatus(404)
+    return res.status(200).send(request)
+  } catch (error) {
+    res.status(500).send({ error })
+  }
 })
 
 module.exports = router
