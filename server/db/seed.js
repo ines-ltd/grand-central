638948bfdfb0a7634cb1963d4df5bb1ae62849aa
si-mv <simon.haines@multiverse.io>
2022-10-23 01:43:50 +0100

    typoes

--- server/db/seed.js
+++ server/db/seed.js
@@ -12,12 +12,12 @@ async function seed () {
   await db.sync({ force: true })
 
   const users = await Promise.all(data.users.map(u => User.create(u)))
-  const requests = await Promise.all(data.requests.map(r => User.create(r)))
+  const requests = await Promise.all(data.requests.map(r => Request.create(r)))
 
   await users[0].setManager(users[1])
   
   await requests[0].setOwner(users[0])
-  await Promise.all(users.map(u => request[0].addSubscriber(u)))
+  await Promise.all(users.map(u => requests[0].addSubscriber(u)))
 
 }
 
