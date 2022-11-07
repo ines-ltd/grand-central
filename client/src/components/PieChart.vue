f4b1b7a491bc614aa6fb9c4389a433f94625de52
si-mv <simon.haines@multiverse.io>
2022-11-07 22:50:14 +0000

    add pie charts

---
+++ client/src/components/PieChart.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { onMounted, ref, defineProps } from 'vue'
+import Chart from 'chart.js/auto'
+
+const el = ref(null)
+
+const props = defineProps({
+  labels: {
+    type: Array,
+    default: []
+  },
+  datasets: {
+    type: Array,
+    default: []
+  },
+  type: {
+    type: String,
+    default: 'pie'
+  },
+  colors: {
+    type: Array,
+    default: ['#150485', '#590995', '#C62A88', '#03C4A1']
+  }
+})
+
+const config = {
+  type: props.type,
+  data: {
+    labels: props.labels,
+    datasets: props.datasets
+  }
+}
+
+onMounted(() => {
+  const chart = new Chart(el.value, config)
+})
+
+</script>
+
+<template>
+  <canvas id="chart" ref="el" />
+</template>
