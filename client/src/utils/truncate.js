3a9652afd78b22e41f5ddd6bb7ce368f2fec54ee
si-mv <simon.haines@multiverse.io>
2022-11-12 15:57:32 +0000

    bug fix

--- client/src/utils/truncate.js
+++ client/src/utils/truncate.js
@@ -6,6 +6,6 @@
  * @param {number} [count=12] - The character limit
  * @returns {string} A truncated string, with elipsis if necessary
  */
-export default function truncate (str, count) {
-  return str.slice(0, count = 12) + (str.length > count ? '...' : '')
+export default function truncate (str, count = 12) {
+  return str.slice(0, count) + (str.length > count ? '...' : '')
 }
